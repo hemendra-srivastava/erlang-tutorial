50dae7e6c61e36e1ccf0a406e06e4bfaa9c2e350
Hemendra <hemendra26@gmail.com>
2016-03-04 01:10:39 +0530

    guards

---
+++ what_the_if.erl
@@ -0,0 +1,49 @@
+-module(what_the_if).
+-compile(export_all). 
+ 
+heh_fine() ->
+    if 1 =:= 1 ->
+            works
+    end,
+    if 1 =:= 2; 1 =:= 1 ->
+            works
+    end,
+    if 1 =:= 2, 1 =:= 1 ->
+            fails
+    end.
+
+oh_god(N) ->
+    if N =:= 2 -> might_succeed;
+       true -> always_does  %% this is Erlang's if's 'else!'
+    end.
+
+help_me(Animal) ->
+    Talk = if Animal == cat  -> "meow";
+              Animal == beef -> "mooo";
+              Animal == dog  -> "bark";
+              Animal == tree -> "bark";
+              true -> "fgdadfgna"
+           end,
+    {Animal, "says " ++ Talk ++ "!"}.
+
+
+insert(X,[]) ->
+    [X];
+insert(X,Set) ->
+    case lists:member(X,Set) of
+        true  -> Set;
+        false -> [X|Set]
+    end.
+
+
+beach(Temperature) ->
+    case Temperature of
+        {celsius, N} when N >= 20, N =< 45 ->
+            'favorable';
+        {kelvin, N} when N >= 293, N =< 318 ->
+            'scientifically favorable';
+        {fahrenheit, N} when N >= 68, N =< 113 ->
+            'favorable in the US';
+        _ ->
+            'avoid beach'
+    end.
