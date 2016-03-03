fe8116356da10a28d6846f766311b86deb783980
Hemendra <hemendra26@gmail.com>
2016-03-04 02:25:59 +0530

    data structs

---
+++ dstruct.erl
@@ -0,0 +1,46 @@
+-module(dstruct).
+
+-compile(export_all).
+
+-record(robot, {name,
+                type=industrial,
+                hobbies,
+                details=[]}).
+
+first_robot() ->
+    #robot{name="Mechatron",
+           type=handmade,
+           details=["Moved by a small man inside"]}.
+
+car_factory(CorpName) ->
+    #robot{name=CorpName, hobbies="building cars"}.
+
+% Crusher = #robot{name="Crusher", hobbies=["Crushing people","petting cats"]}.
+% Crusher#robot.hobbies.
+
+% NestedBot = #robot{details=#robot{name="erNest"}}.
+
+% NestedRobot#robot.details#robot.name
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-record(user, {id, name, group, age}).
+
+%% use pattern matching to filter
+admin_panel(#user{name=Name, group=admin}) ->
+    Name ++ " is allowed!";
+admin_panel(#user{name=Name}) ->
+    Name ++ " is not allowed".
+
+%% can extend user without problem
+adult_section(U = #user{}) when U#user.age >= 18 ->
+    %% Show stuff that can't be written in such a text
+    allowed;
+adult_section(_) ->
+    %% redirect to sesame street site
+    forbidden.
+
+repairman(Rob) ->
+    Details = Rob#robot.details,
+    NewRob = Rob#robot{details=["Repaired by repairman"|Details]},
+    {repaired, NewRob}.
